0a3ec339f5ed241e085273f4f132aa087755efa8
LeeviPelkonen <32296789+LeeviPelkonen@users.noreply.github.com>
2019-09-26 21:51:08 +0300

    class structures

--- app/src/main/java/com/example/puhelinluettelo/MainActivity.java
+++ app/src/main/java/com/example/puhelinluettelo/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.puhelinluettelo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        //MainAdapter adapter = new MainAdapter(this,)
+        //listView.setAdapter();
+    }
+
+    private String getJson(){
+        String json = null;
+        try {
+            InputStream inputStream = getAssets().open("contacts.json");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+    private List<Contact> parseJson(){
+        List<Contact> items = null;
+
+        return items;
     }
 }
--- app/src/main/java/com/example/puhelinluettelo/MainAdapter.java
+++ app/src/main/java/com/example/puhelinluettelo/MainAdapter.java
@@ -1,8 +1,55 @@
 package com.example.puhelinluettelo;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
 import java.util.List;
 
-public class MainAdapter {
+import androidx.annotation.NonNull;
 
-    List<String> nameList;
+class MainAdapter extends ArrayAdapter<Contact> {
+
+    private Boolean firstNameFirst;
+    private Context context;
+    private List<Contact> contactList;
+
+    MainAdapter(Context c, List<Contact> contactList){
+        super(c, R.layout.list_item, R.id.itemButton);
+
+        this.context = c;
+        this.contactList = contactList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Contact contact = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+        Button name = convertView.findViewById(R.id.itemButton);
+
+        if(firstNameFirst){
+            name.setText(contact.getFirstName() + " " + contact.getLastName());
+        }else{
+            name.setText(contact.getLastName() + " " + contact.getFirstName());
+        }
+
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Contact myContact = (Contact) view.getTag();
+                //TODO use the contact
+                Log.d("testing",myContact.getFirstName());
+            }
+        });
+
+        return convertView;
+    }
 }
